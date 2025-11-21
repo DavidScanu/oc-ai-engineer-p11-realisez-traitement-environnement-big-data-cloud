aa058afe3d597c83456d6f46a886a0d283830e99
David Scanu <DavidScanu@users.noreply.github.com>
2025-11-21 10:51:33 +0000

    chore: 🗑️ Nettoyage fichiers obsolètes
    Suppression des fichiers liés aux approches abandonnées :
    
    1. documentation/ (guides JupyterHub/EMR Studio non fonctionnels)
    2. jupyterhub_config_*.py (configs JupyterHub inutilisées)
    3. Scripts obsolètes à la racine :
       - create_cluster.sh, monitor_cluster.sh, terminate_cluster.sh
       - install_dependencies.sh (anciennes versions)
    4. Scripts non utilisés dans scripts/ :
       - aws_emr_studio_setup.sh, aws_setup.sh
       - convert_notebook_to_emr.py, migrate_config.sh, update_aws_setup.py
    
    Approche finale retenue : EMR Steps + scripts bash (traitement/)
    
    Conservation :
    - traitement/etape_1/ et etape_2/ (pipelines fonctionnels)
    - notebooks/ (développement local)
    - scripts/aws_audit.sh (seul utilitaire conservé)

--- set_jupyter_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo sed -i "/^c.Spawner.environment = {$/,\$d" /etc/jupyter/conf/jupyterhub_config.py && echo "c.Spawner.environment = {
-    'SPARKMAGIC_CONF_DIR': '/etc/jupyter/conf',
-    'JUPYTER_ENABLE_LAB': 'yes',
-    'S3_ENDPOINT_URL': 's3.eu-west-1.amazonaws.com',
-    'AWS_REGION': 'eu-west-1',
-    'AWS_DEFAULT_REGION': 'eu-west-1'
-}" | sudo tee -a /etc/jupyter/conf/jupyterhub_config.py && sudo docker restart jupyterhub
